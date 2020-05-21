10f271dbe58d8597c9aa235325b8a3c24ef47b92
José Carneiro <josemcarneiro@gmail.com>
2020-05-21 19:21:35 +0100

    Removed unecessary react-client directory

--- react-client/src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  apiKey: 'AIzaSyAyXZGjAc-AU8uaOmcdseCDPGUiUC_Gj2I',
-  // authDomain: '{YOUR KEY}',
-  databaseURL: 'https://transportes-live.firebaseio.com'
-};
-
-export { config as firebaseConfiguration };
--- react-client/src/services/realtime-database.js
+++
@@ -1,8 +0,0 @@
-import Firebase from 'firebase';
-import { firebaseConfiguration } from './../config';
-
-const app = Firebase.initializeApp(firebaseConfiguration);
-
-const database = app.database();
-
-export default database;

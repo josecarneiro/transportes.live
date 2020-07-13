d36fcaa8d04c63508b4c0c374ef5609e9e270fcc
José Carneiro <josemcarneiro@gmail.com>
2020-07-13 18:54:21 +0100

    Fixed order of constants

--- client/constants.js
+++ client/constants.js
@@ -4,15 +4,15 @@ const { NODE_ENV, URL = 'https://transportes.live', NETLIFY } = process.env;
 
 const BUILD_ENVIRONMENT = NETLIFY ? 'remote' : 'local';
 
+const ORIGIN = URL.replace(/https:\/\//, '');
+const NAME = ORIGIN;
+
 const ENVIRONMENT =
   NODE_ENV === 'development'
     ? 'development'
     : ORIGIN.includes('staging')
     ? 'staging'
     : 'production';
-
-const ORIGIN = URL.replace(/https:\/\//, '');
-const NAME = ORIGIN;
 
 module.exports = {
   ENVIRONMENT,
